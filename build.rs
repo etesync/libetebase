762778821cefb9a8093000a52d32175bbf485760
Skycoder42 <Skycoder42@users.noreply.github.com>
2023-01-03 20:42:35 +0100

    Improve macos support

--- build.rs
+++ build.rs
@@ -7,7 +7,7 @@ use std::env;
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    if cfg!(unix) {
+    if cfg!(unix) && !cfg!(macos) {
         // Set soname for platforms other than Windows
         const VERSION_MAJOR: &'static str = env!("CARGO_PKG_VERSION_MAJOR");
         println!("cargo:rustc-cdylib-link-arg=-Wl,-soname,libetebase.so.{}", VERSION_MAJOR);
